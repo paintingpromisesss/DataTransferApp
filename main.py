1e685495c58c37d66202baa0fd692e325c466edf
paintingpromisesss <paintingpromisesss@7yndx.ru>
2025-04-09 19:46:24 +0300

    updater ready 95%

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from flet import app
 from src.ui.ui_script import UIScript
+from updater import Updater
+from sys import argv
 
-app(UIScript)
+
+if __name__ == "__main__":
+    if len(argv) > 1:
+        if argv[1] == "--no_update":
+            app(UIScript)
+    else:
+        updater = Updater()
+        update_triggered = updater.run_updater(argv[0])
+        if not update_triggered:
+            app(UIScript)
--- updater.py
+++ updater.py
@@ -1,21 +1,22 @@
-import json
-import requests
+from json import load
+from requests import get
 from packaging import version
-import os
+from os import path
+from sys import argv, exit
+from subprocess import run, DETACHED_PROCESS
 from src.config_system.utility import Utility
-import sys
-import subprocess
+
 
 
 class Updater:
     def __init__(self):
         with open("app_info.json", "r") as f:
-            self.info = json.load(f)
-            self.url = f"https://api.github.com/repos/{self._get_origin()}/releases/latest"
+            self.info = load(f)
+        self.url = f"https://api.github.com/repos/{self.__get_origin()}/releases/latest"
 
-    def _get_latest_version(self):
+    def __get_latest_version(self):
         try:
-            response = requests.get(self.url)
+            response = get(self.url)
             response.raise_for_status()
             data = response.json()
             latest_version = data["tag_name"]
@@ -24,17 +25,17 @@ class Updater:
             print(f"Error fetching version: {e}")
             return None
 
-    def _get_current_version(self):
+    def __get_current_version(self):
         return self.info["version"]
 
-    def _get_origin(self):
+    def __get_origin(self):
         return self.info["origin"]
 
-    def _is_update_available(self):
+    def is_update_available(self):
         try:
-            latest_version = self._get_latest_version()
+            latest_version = self.__get_latest_version()
             if latest_version:
-                current_version = self._get_current_version()
+                current_version = self.__get_current_version()
                 if version.parse(latest_version) > version.parse(current_version):
                     return True
                 else:
@@ -45,10 +46,30 @@ class Updater:
         except Exception as e:
             print(f"Error comparing versions: {e}")
             return False
+    
+    def build_bat(self, current_executable, new_executable : str) -> str:
+        temp_dir = Utility.get_temp_directory()
+        backup_executable = path.join(current_executable + ".backup")
+        bat_path = path.join(temp_dir, "update_script.bat")
 
-    def _download_update(self):
+        bat_script = f"""@echo off
+timeout /t 1 /nobreak >nul
+del "{backup_executable}" 2 >nul
+ren "{current_executable}" "{path.basename(backup_executable)}"
+copy "{new_executable}" "{current_executable}"
+start "" "{current_executable}" --no_update
+del "{new_executable}"
+del "{bat_path}"
+"""
+        
+        with open(bat_path, "w") as bat_file:
+            bat_file.write(bat_script)
+        return bat_path
+
+
+    def download_update(self):
         try:
-            response = requests.get(self.url)
+            response = get(self.url)
             response.raise_for_status()
             data = response.json()
             download_url = None
@@ -65,13 +86,12 @@ class Updater:
             if download_url:
 
                 temp_dir = Utility.get_temp_directory()
-                file_name = os.path.join(
+                file_name = path.join(
                     temp_dir, f"DataTransferApp_update_{data['tag_name']}.exe")
 
-                # Download the file
                 print(f"Downloading update to {file_name}...")
 
-                with requests.get(download_url, stream=True) as r:
+                with get(download_url, stream=True) as r:
                     r.raise_for_status()
                     with open(file_name, 'wb') as f:
                         for chunk in r.iter_content(chunk_size=8192):
@@ -86,3 +106,36 @@ class Updater:
         except Exception as e:
             print(f"Error downloading update: {e}")
             return None
+    
+    def run_updater(self, current_executable) -> None:
+        try:
+            if self.__is_update_available():
+                print('Update available')
+                file_name = self.__download_update()
+                if file_name:
+                    bat_path = self.__build_bat(current_executable, file_name)
+                    print(f"Running update script: {bat_path}")
+                    command = f'Start-Process -FilePath "{bat_path}" -Verb RunAs'
+                    run(["powershell", "-Command", command], shell=True, creationflags=DETACHED_PROCESS)
+                    exit(0)
+                    return True
+        except Exception as e:
+            print(f"Error in updater: {e}")
+            return False
+    
+    def cleanup(self) -> None:
+        try:
+            temp_dir = Utility.get_temp_directory()
+            bat_path = path.join(temp_dir, "update_script.bat")
+            if path.exists(bat_path):
+                path.remove(bat_path)
+                print(f"Removed temporary file: {bat_path}")
+        except Exception as e:
+            print(f"Error cleaning up: {e}")
+
+updater = Updater()
+if updater.is_update_available():
+                print('Update available')
+                file_name = updater.download_update()
+                if file_name:
+                    bat_path = updater.build_bat(argv[0], file_name)
